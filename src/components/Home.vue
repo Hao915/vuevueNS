d20bf8f20246603ca4a46c4287d04cb537b7cc3c
Hao <lihao.915@gmail.com>
2018-06-08 17:33:18 +1000

    Some change

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,42 @@
 <template>
   <Page class="page">
-    <ActionBar class="action-bar" title="Home"/>
-
+    <!-- <ActionBar class="action-bar" title="Home" backgroundColor="black"/> -->
     <StackLayout>
-      <Button class="btn btn-primary" @tap="$router.push('/counter')">Counter</Button>
-      <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World</Button>
+      <Image src="~/images/NativeScript-Vue.png"/>
+    </StackLayout>
+    <StackLayout class="bottom">
+      <!-- <Button class="btn btn-primary" @tap="$router.push('/counter')">Counter</Button> -->
+      <Button class="button-copy" @tap="$router.push('/hello')">登录</Button>
+      <Button class="link">联系客服</Button>
     </StackLayout>
 
   </Page>
 </template>
+
+<script>
+
+</script>
+ 
+<style scoped>
+  .bottom{
+    margin-top: 70%;
+  }
+  .button-copy{
+  color:black;
+  background-color: goldenrod;
+  border-radius: 20px;
+  font-weight: bold;
+  }
+  .btn-primary{
+    color:goldenrod;
+  }
+  .label-text{
+    color:goldenrod;
+  }
+  /* From platform.ios.css */
+.link {
+    border-width: 0;
+    color:goldenrod;
+}
+</style>
+
